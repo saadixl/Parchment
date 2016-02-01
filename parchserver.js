cd590086b7a77e488cd80dc1279272b84ae7942e
Masudul Haque <hi@masud.pw>
2016-02-01 13:10:24 +0600

    changing view

--- parchserver.js
+++ parchserver.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('view engine', 'jade');
 var URL = require('./parch_modules/mong');
 require('./parch_modules/routes.js')(app, URL);
-app.use(express.static(__dirname + '../public'));
+app.use(express.static(__dirname + '/public'));
 app.listen(8800, function() {
     console.log("Parchment server started at port 8800");
 });
