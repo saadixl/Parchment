2a33ed9a7c6bbbc31ca48fc1b7332e7e441e3a04
Masudul Haque <hi@masud.pw>
2016-02-01 10:58:35 +0600

    commiting parch modules

---
+++ parch_modules/mong.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var shortid = require('shortid');
+mongoose.connect('mongodb://localhost/parchment');
+
+
+var urlSchema = new mongoose.Schema({
+    handle: {
+        type: String,
+        unique: true,
+        'default': shortid.generate
+    },
+    markUp: {
+        type: String
+    }
+});
+
+// Creating a new model using the schema
+var URL = mongoose.model('URL', urlSchema);
+
+// Exporting the model
+module.exports = URL;
---
+++ parch_modules/routes.js
@@ -0,0 +1,44 @@
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
+});
+
+module.exports = function(app, URL) {
+    app.get('/', urlencodedParser, function(req, res) {
+        // Creating a new url record
+        var newURL = new URL({
+            markUp: ""
+        });
+        newURL.save(function(err, doc) {
+            if (err) throw err;
+            console.log('URL saved successfully!' + doc.handle);
+            res.redirect('http://localhost:8800/' + doc.handle);
+        });
+    });
+
+    app.get('/getHandle', urlencodedParser, function(req, res) {
+        URL.find({
+                handle: req.handle
+            },
+            // if exists then fetch the mark ups
+            function(err, docs) {
+                if (docs.length == 1) {
+                    res.send(docs[0].markUp);
+                }
+            });
+    });
+
+    app.post('/save', urlencodedParser, function(req, res) {
+        Model.findOne({
+            handle: req.body.handle
+        }, function(err, doc) {
+            doc.markUp = req.body.markUp;
+            doc.save();
+        });
+    });
+
+    app.get('/:handle', urlencodedParser, function(req, res) {
+        res.render('../public/index');
+    });
+
+}
