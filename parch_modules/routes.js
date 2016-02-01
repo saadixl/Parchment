a8e5559481daec31b02e164c655bc2603157ad03
Masudul Haque <hi@masud.pw>
2016-02-01 13:30:35 +0600

    changing views 1

--- parch_modules/routes.js
+++ parch_modules/routes.js
@@ -43,7 +43,7 @@ module.exports = function(app, URL) {
     });
 
     app.get('/:handle', urlencodedParser, function(req, res) {
-        res.render('../public/views/index');
+        res.render(__dirname + '../public/views/index');
     });
 
 }
