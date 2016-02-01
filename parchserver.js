f525d7d92a24938777ca1c3fe57e2bbcf4d958bd
Masudul Haque <hi@masud.pw>
2016-02-01 13:38:27 +0600

    changing views 2

--- parchserver.js
+++ parchserver.js
@@ -2,6 +2,8 @@ var serveStatic = require('serve-static');
 var express = require('express');
 var app = express();
 app.set('view engine', 'jade');
+app.set('views', __dirname + '/public/views');
+
 var URL = require('./parch_modules/mong');
 require('./parch_modules/routes.js')(app, URL);
 app.use(express.static(__dirname + '/public'));
